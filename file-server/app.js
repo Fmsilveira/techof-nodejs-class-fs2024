a21cbb7f6704c35687b6fcbc7d04c24532b4e8fa
Bruno Hirata <brn.hirata@gmail.com>
2024-12-12 20:02:26 +0000

    step2

--- file-server/app.js
+++ file-server/app.js
@@ -2,6 +2,8 @@ const http = require("http");
 const fs = require("fs").promises;
 const path = require("path");
 
+const USERS = [];
+
 const getPathFromUrl = (url) => {
   const requestUrl = new URL(url, 'http://localhost:3000/');
   return requestUrl.pathname;
@@ -21,12 +23,10 @@ const getCss = async (name) => {
   return await getFileFromPath(cssPath);
 }
 
-const createUser = () => {
-  const html = `  <div id="users">
-    <div>
-      <p><span style="font-weight: bold;">First Name</span>$FIRST_NAME</p>
-      <p><span style="font-weight: bold;">Last Name</span>$LAST_NAME</p>
-    </div>
+const createUser = (firstName, lastName) => {
+  const html = `  <div class="user">
+      <p><span style="font-weight: bold;">First Name: </span>$FIRST_NAME</p>
+      <p><span style="font-weight: bold;">Last Name: </span>$LAST_NAME</p>
   </div>`;
 
   return html.replaceAll('$FIRST_NAME', firstName).replaceAll('$LAST_NAME', lastName);
@@ -51,8 +51,12 @@ const server = http.createServer(
 
         if ('GET' === method) {
           const index = await getIndexPage();
+
+          const usersHtml = USERS.map(user => createUser(user.firstName, user.lastName));
+
           response.writeHead(200, { "Content-Type": "text/html" });
-          response.write(index.toString());
+          response.write(index.toString().replaceAll('{%USERS%}', usersHtml.join('\n')));
+
           response.end();
         } else {
           let body = '';
@@ -61,9 +65,18 @@ const server = http.createServer(
           });
           request.on('end', async () => {
             console.log(body);
+            const data = {};
+            body.split('&').map(param => param.split('=')).forEach(([key, value]) => data[key] = value);
+            USERS.push({
+              firstName: data['first-name'],
+              lastName: data['last-name']
+            });
+
+            const usersHtml = USERS.map(user => createUser(user.firstName, user.lastName));
+
             const index = await getIndexPage();
             response.writeHead(200, { "Content-Type": "text/html" });
-            response.write(index.toString());
+            response.write(index.toString().replaceAll('{%USERS%}', usersHtml.join('\n')));
             response.end();
           })
           break;
